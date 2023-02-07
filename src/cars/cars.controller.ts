b1a8bdeac1ad0ffb2ceff8c91e964d3b171b80c8
Patrick Tanta <43278282+PatrickTanta@users.noreply.github.com>
2023-02-07 09:29:00 -0500

    feat: adding prettier.json config

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,20 +1,43 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, ParseUUIDPipe, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CarsService } from './cars.service';
+import { CreateCarDto } from './dto/create-car.dto';
 
 @Controller('cars')
+@UsePipes(ValidationPipe)
 export class CarsController {
   constructor (
     private readonly carsService: CarsService
   ) {}
 
   @Get()
-  getAllCars() {
+  getAllCars () {
     return this.carsService.findAll();
   }
 
   @Get(':id')
-  getCardById(@Param('id', ParseIntPipe) id: string) {
+  getCardById (@Param('id', ParseUUIDPipe) id: string) {
 
-    return this.carsService.findCarById(+id);
+    return this.carsService.findCarById(id);
+  }
+
+  @Post()
+  createCar (@Body() createCarDto: CreateCarDto) {
+    return createCarDto;
+  }
+
+  @Patch(':id')
+  updateCar (
+    @Param('id', ParseIntPipe) id: string,
+    @Body() body: any)
+  {
+    return body;
+  }
+
+  @Delete(':id')
+  deleteCar (@Param('id', ParseIntPipe) id: string) {
+    return {
+      method: 'delete',
+      id
+    };
   }
 }
